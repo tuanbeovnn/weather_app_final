e6a36643a6c6565564d901c6b61b58377bf125ec
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-12-05 20:32:22 +0200

    get unique favorite location

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/repository/WeatherHistoryRepository.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/repository/WeatherHistoryRepository.java
@@ -12,7 +12,17 @@ public interface WeatherHistoryRepository extends JpaRepository<WeatherHistoryEn
     @Query(value = "select * from weather_history wh order by wh.created_date desc limit 6", nativeQuery = true)
     List<WeatherHistoryEntity> findAllByCreatedDate();
 
-    @Query(value = "select * from weather_history wh where wh.is_favorite = true order by wh.created_date desc limit 6", nativeQuery = true)
+    @Query(value = "SELECT wh.*\n" +
+        "FROM weather_history wh\n" +
+        "JOIN (\n" +
+        "    SELECT location, MAX(created_date) AS max_created_date\n" +
+        "    FROM weather_history\n" +
+        "    WHERE is_favorite = true\n" +
+        "    GROUP BY location\n" +
+        ") max_dates\n" +
+        "ON wh.location = max_dates.location AND wh.created_date = max_dates.max_created_date\n" +
+        "WHERE wh.is_favorite = true\n" +
+        "ORDER BY wh.created_date DESC\n" +
+        "LIMIT 6", nativeQuery = true)
     List<WeatherHistoryEntity> findAllByFavoriteCreatedDate();
-
 }
